853b925d03490eb059d09dd8d6eb88543d4119c3
timtnlee <timtnlee1995@gmail.com>
2017-11-02 22:00:30 +0800

    first commit
    first commit, with 4 songs

---
+++ source.js
@@ -0,0 +1,28 @@
+var prifix="music/";
+var sourceList=[{
+	name:'記得',
+	singer:'張惠妹',
+	src:prifix+'mei.mp3',
+	img:'img/mei-1.jpg'
+},{
+	name:'愛笑的眼睛',
+	singer:'林俊傑',
+	src:prifix+'林俊傑 愛笑的眼睛.mp3',
+	img:'img/lin-1.jpg'
+},{
+	name:'演員',
+	singer:'薛之謙',
+	src:prifix+'薛之謙-演員.mp3',
+	img:'img/joker-1.jpg'
+},{
+	name:'關鍵詞',
+	singer:'林俊傑',
+	src:prifix+'林俊傑-關鍵詞.mp3',
+	img:'img/lin-2.jpg'
+},{
+	name:'三月',
+	singer:'張惠妹',
+	src:prifix+'張惠妹-三月.mp3',
+	img:'img/mei-3.jpg'
+}
+]
